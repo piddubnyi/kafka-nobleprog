6d6e17bdaafaa7f229a73aa24d678977a1e35932
Igor Piddubnyi <igor.piddubnyi@solvians.com>
2020-12-17 09:22:49 +0100

    Add simple kafka streams

---
+++ src/main/java/com/nobleprog/kafka/SimpleKafkaStreams.java
@@ -0,0 +1,31 @@
+package com.nobleprog.kafka;
+
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@SpringBootApplication
+@EnableKafkaStreams
+public class SimpleKafkaStreams {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleKafkaStreams.class, args);
+    }
+
+    @Configuration
+    public static class KafaStreamsConfig {
+
+        @Bean
+        public KStream<Long, String> dayAggregation(StreamsBuilder streamsBuilder) {
+            final KStream<Long, String> inputStream = streamsBuilder.stream("my-topic");
+            //merge
+            inputStream.to("copy-topic");
+            return inputStream;
+
+        }
+    }
+}
